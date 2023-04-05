1732a8ed149da71d4e09d700d06bc0d457e47667
yazimel.niclis <yazimelniclis1995@gmail.com>
2023-04-05 09:31:47 -0300

    conflictos

--- src/App.jsx
+++ src/App.jsx
@@ -18,6 +18,7 @@ function App() {
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
+        <div>hola vengo a molestar</div>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
