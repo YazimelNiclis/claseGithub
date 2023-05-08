24755ac6651dafb25ee8e8c8b2e45499cc3e1d4f
yazimel.niclis <yazimelniclis1995@gmail.com>
2023-05-08 09:32:21 -0300

    cambios yazi

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
-      {/* <div className="card">
+      <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
@@ -22,7 +22,7 @@ function App() {
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
-      </div> */}
+      </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
